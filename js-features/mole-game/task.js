80492ef2d5a8b7ff19af3074cbc4c25705f3c954
Daria Tatarinova <dt.dashenka@mail.ru>
2022-11-06 16:24:36 +1000

    second commit mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,32 +3,36 @@ const lost = document.getElementById("lost");
 let countDead = 0;
 let countLost = 0;
 
-getHole = index => document.getElementById(`hole${index}`);
-for(let i = 0; i < 10; i++){
-    let hole = getHole(i);
-    hole.onclick = function (){
-        if(hole.className.includes('hole_has-mole')){
-            countDead += 1;
-            dead.textContent = countDead;
-        } else {
-            countLost += 1;
-            lost.textContent = countLost;
-        }
-        if(countDead == 10){
-            alert("Победа");
-            countDead = 0;
-            countLost = 0;
-            dead.textContent = countDead;
-            lost.textContent = countLost;
-        }
-        if(countLost == 5){
-            alert("Проигрыш");
-            countDead = 0;
-            countLost = 0;
-            dead.textContent = countDead;
-            lost.textContent = countLost;
-        }
+
+let hole = document.getElementsByClassName("hole");
+let holeArr = [...hole];
+function clickHalder (){
+    if(this.className.includes('hole_has-mole')){
+        countDead += 1;
+        dead.textContent = countDead;
+    } else {
+        countLost += 1;
+        lost.textContent = countLost;
+    }
+    if(countDead == 10){
+        alert("Победа");
+        countDead = 0;
+        countLost = 0;
+        dead.textContent = countDead;
+        lost.textContent = countLost;
+    }
+    if(countLost == 5){
+        alert("Проигрыш");
+        countDead = 0;
+        countLost = 0;
+        dead.textContent = countDead;
+        lost.textContent = countLost;
     }
 }
+holeArr.forEach((item) => {
+    item.onclick = clickHalder;
+})
+
+
 
 
