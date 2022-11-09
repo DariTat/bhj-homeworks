d36e35c9ac3d2250c713f4f39b74bd082f481ae0
Daria Tatarinova <dt.dashenka@mail.ru>
2022-11-09 21:57:21 +1000

    first commit popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,21 @@
+
+const close = document.getElementsByClassName('modal__close');
+let closeArray = Array.from(close);
+const modal = document.getElementById('modal_main');
+modal.classList.add('modal_active');
+const modalSuccess = document.getElementById('modal_success');
+
+function modalClose(){
+    if(this.className.includes('modal__close_times')){
+        modal.style.display = 'none';
+        modalSuccess.style.display = 'none';
+    }
+}
+closeArray.forEach((item) => item.onclick = modalClose);
+
+const showSuccess = document.getElementsByClassName('show-success');
+let showArray = [...showSuccess];
+
+showArray[0].onclick = function(){
+    modalSuccess.style.display = 'flex';
+}
