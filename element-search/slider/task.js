98697b43adcf31eade1329ae8ebf0bc476d361cf
Daria Tatarinova <dt.dashenka@mail.ru>
2022-11-11 15:35:49 +1000

    second commit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -5,27 +5,16 @@ let slider = document.querySelectorAll('.slider__item');
 let sliderArr = Array.from(slider);
 let indexSlider = 0;
 function next(){ 
-    if(indexSlider < (sliderArr.length - 1)){
-        sliderArr[indexSlider].classList.remove('slider__item_active');
-        indexSlider += 1;
-        sliderArr[indexSlider].classList.add('slider__item_active');
-        
-    }else if(indexSlider == (sliderArr.length - 1)) {
-        sliderArr[indexSlider].classList.remove('slider__item_active');
-        indexSlider = 0;
-        sliderArr[indexSlider].classList.add('slider__item_active');    
-    } 
+    indexSlider = sliderArr.findIndex((item) => item.className.includes('slider__item_active'));
+    sliderArr[indexSlider].classList.remove('slider__item_active');
+    (indexSlider < (sliderArr.length - 1))? ++indexSlider : indexSlider = 0;
+    sliderArr[indexSlider].classList.add('slider__item_active');  
 }
 function prev(){
-    if(indexSlider == 0){
-        sliderArr[indexSlider].classList.remove('slider__item_active');
-        indexSlider = sliderArr.length - 1;
-        sliderArr[indexSlider].classList.add('slider__item_active');   
-    } else {
-        sliderArr[indexSlider].classList.remove('slider__item_active');
-        indexSlider -= 1;
-        sliderArr[indexSlider].classList.add('slider__item_active');
-    }
+    indexSlider = sliderArr.findIndex((item) => item.className.includes('slider__item_active'));
+    sliderArr[indexSlider].classList.remove('slider__item_active');
+    indexSlider == 0 ? indexSlider = sliderArr.length - 1 : --indexSlider
+    sliderArr[indexSlider].classList.add('slider__item_active');   
 }
 arrowNext.onclick = next;
 arrowPrev.onclick = prev;
