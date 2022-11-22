966f39367c506cd53078c9318308f222f5afa0a4
Daria Tatarinova <dt.dashenka@mail.ru>
2022-11-22 12:42:30 +1000

    first commit chat

---
+++ html-forms/chat/task.js
@@ -0,0 +1,44 @@
+const widget = document.querySelector('.chat-widget');
+const message = document.querySelector('.chat-widget__messages');
+const input = document.getElementById('chat-widget__input');
+
+widget.addEventListener('click', () => {
+    widget.classList.add('chat-widget_active');
+    
+})
+let timeNow = new Date().toLocaleTimeString("ru-Ru", {
+    hour: "2-digit",
+    minute: "2-digit",
+    });
+
+function submit(){
+    if(input.value === "") {
+        return false;
+    }
+    message.innerHTML += `
+        <div class="message message_client">
+        <div class="message__time">${timeNow}</div>
+        <div class="message__text">${input.value}</div>
+        </div>
+    `;
+}
+function submitRobot(){
+    if(input.value === "") {
+        return false;
+    }
+    let value = ['Здравстуйте', 'Добрый день! Дождитесь ответа оператора.', 'Я не хочу с Вами общаться. До свидания!', 'Данный абонент не доступен.']
+    let rand = Math.floor(Math.random() * value.length);
+    message.innerHTML += `
+    <div class="message">
+    <div class="message__time">${timeNow}</div>
+    <div class="message__text">${value[rand]}</div>
+    </div>
+`;
+}  
+document.addEventListener('keyup', function(e){
+    if (e.keyCode === 13) {
+        submit();
+        submitRobot();
+       
+    }
+}) 
