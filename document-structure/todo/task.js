ca2580cc92a51780502a8f3d0269360cc46e41f8
Daria Tatarinova <dt.dashenka@mail.ru>
2022-11-27 22:29:48 +1000

    second commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,21 +3,20 @@ let list = document.querySelector('.tasks__list');
 let form = document.querySelector('.tasks__control');
 
 form.onsubmit = function(){
+    input.value = input.value.trim();
     if(input.value == ''){
         return false;
     }
-    list.innerHTML += `
+    list.insertAdjacentHTML ("afterbegin",  `
     <div class="task">
     <div class="task__title">${input.value}</div>
     <a href="#" class="task__remove">x</a>
-    </div>`
+    </div>`);
     form.reset();
-    let remove = Array.from(document.querySelectorAll('.task__remove'));
-    
-    remove.forEach((item)=> item.addEventListener('click', ()=>{
-        item.parentElement.remove();
-    }) )
-    return false;
+    let remove = document.querySelector('.task__remove');
+    remove.addEventListener('click', ()=>{
+        remove.parentElement.remove();
+    }) 
 }
 
 
