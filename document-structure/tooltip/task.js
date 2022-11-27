b16d1165dc03c0ad296d29663af3b1867be8d5f1
Daria Tatarinova <dt.dashenka@mail.ru>
2022-11-27 22:26:09 +1000

    second commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,25 @@
 const tooltips = Array.from(document.querySelectorAll('.has-tooltip'));
 const links = Array.from(document.querySelectorAll('a'));
 
+let tooltip = document.createElement('div');
+tooltip.className = 'tooltip';
+
 tooltips.forEach((item) => item.addEventListener('click', ()=>{
     const {height, top, left} = item.getBoundingClientRect();
-    item.outerHTML += `<div class="tooltip">${item.title}</div>`;
-    let tooltip =  document.querySelector('.tooltip');
-    tooltip.classList.toggle('tooltip_active');
-    tooltip.style.top = (top + height) + "px";
-    tooltip.style.left = left + "px";
+    if(tooltip.innerText == item.title){
+        tooltip.classList.toggle('tooltip_active');
+    } else {
+        tooltip.innerText = item.title;
+        tooltip.classList.add('tooltip_active');
+        tooltip.style.top = (top + height) + "px";
+        tooltip.style.left = left + "px";
+        item.insertAdjacentElement("afterend", tooltip);
+    }
 }))
 
 links.forEach((link) => link.onclick = function(){
-    return false;
+    if(link.title != ''){
+        return false;
+    }
+    
 });
